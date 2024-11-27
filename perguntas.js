12ccf160ee0b7674395a69a5eb3780be246d8869
Laura <00001106114905sp@al.educacao.sp.gov.br>
2024-11-27 09:39:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Qual é seu ambiente de desenvolvimento favorito?',
+    'Visual Studio Code, IntelliJ IDEA ou Sublime Text'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são as principais características da região polar?',
+    'Clima frio, gelo, neve e importância para o equilíbrio climático global'
+)
+
+criaCartao(
+    'Programação',
+    'Qual a vantagem do JavaScript?',
+    'Permite desenvolvimento de aplicações web dinâmicas'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como explicar a diferença entre Will e Would?',
+    'Will indica futuro certo, enquanto Would expressa condicional ou futuro incerto'
+)
